49e377e95f3954709588c20b77cd1bba263f5e1b
gavin <gavindeane1@gmail.com>
2025-11-24 16:20:45 -0500

    disabled compiling cudagraphs to get around negative buffer failure inside TorchInductor
    Signed-off-by: gavin <gavindeane1@gmail.com>

--- benchmarks/disagg_benchmarks/disagg_prefill_mode_benchmark.sh
+++ benchmarks/disagg_benchmarks/disagg_prefill_mode_benchmark.sh
@@ -28,6 +28,7 @@ wait_for_server() {
 
 launch_disagg_prefill_baseline() {
   model="Qwen/Qwen2.5-1.5B-Instruct"
+  VLLM_COMPILE_CUDAGRAPH_MODE=none
   CUDA_VISIBLE_DEVICES=0 vllm serve $model \
     --port 8100 \
     --max-model-len 10000 \
@@ -50,6 +51,7 @@ launch_disagg_prefill_baseline() {
 
 launch_disagg_prefill_prefillmode() {
   model="Qwen/Qwen2.5-1.5B-Instruct"
+  VLLM_COMPILE_CUDAGRAPH_MODE=none
   CUDA_VISIBLE_DEVICES=0 vllm prefill $model \
     --port 8100 \
     --max-model-len 10000 \
