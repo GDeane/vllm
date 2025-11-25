c975c2f38b9937891407fe087215e7e9bfab1992
gavin <gavindeane1@gmail.com>
2025-11-25 13:12:53 -0500

    Trying lowering max model length to avoid torchimputer error
    Signed-off-by: gavin <gavindeane1@gmail.com>

--- benchmarks/disagg_benchmarks/disagg_prefill_mode_benchmark.sh
+++ benchmarks/disagg_benchmarks/disagg_prefill_mode_benchmark.sh
@@ -30,15 +30,14 @@ launch_disagg_prefill_baseline() {
   model="Qwen/Qwen2.5-1.5B-Instruct"
   CUDA_VISIBLE_DEVICES=0 vllm serve $model \
     --port 8100 \
-    --max-model-len 10000 \
-    --enforce-eager \
+    --max-model-len 4096 \
     --gpu-memory-utilization 0.6 \
     --kv-transfer-config \
     '{"kv_connector":"SharedStorageConnector","kv_role":"kv_producer","kv_rank":0,"kv_parallel_size":2,"kv_buffer_size":5e9}' &
 
   CUDA_VISIBLE_DEVICES=1 vllm serve $model \
     --port 8200 \
-    --max-model-len 10000 \
+    --max-model-len 4096 \
     --gpu-memory-utilization 0.6 \
     --kv-transfer-config \
     '{"kv_connector":"SharedStorageConnector","kv_role":"kv_consumer","kv_rank":1,"kv_parallel_size":2,"kv_buffer_size":5e9}' &
@@ -53,15 +52,14 @@ launch_disagg_prefill_prefillmode() {
   model="Qwen/Qwen2.5-1.5B-Instruct"
   CUDA_VISIBLE_DEVICES=0 vllm prefill $model \
     --port 8100 \
-    --max-model-len 10000 \
-    --enforce-eager \
+    --max-model-len 4096 \
     --gpu-memory-utilization 0.6 \
     --kv-transfer-config \
     '{"kv_connector":"SharedStorageConnector","kv_role":"kv_producer","kv_rank":0,"kv_parallel_size":2,"kv_buffer_size":5e9}' &
 
   CUDA_VISIBLE_DEVICES=1 vllm serve $model \
     --port 8200 \
-    --max-model-len 10000 \
+    --max-model-len 4096 \
     --gpu-memory-utilization 0.6 \
     --kv-transfer-config \
     '{"kv_connector":"SharedStorageConnector","kv_role":"kv_consumer","kv_rank":1,"kv_parallel_size":2,"kv_buffer_size":5e9}' &
